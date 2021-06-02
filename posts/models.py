2767f701e1c85b31db41c2c11fe9d77032764d86
Macachor, Jerald Louie (Contractor) <JeraldLouie_Macachor@comcast.com>
2021-06-01 17:58:17 -0700

    added post and list view

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
-  text = models.TextField
+  text = models.TextField(default='')
+
+  def __str__(self):
+    return f"Post {self.text[:50]}"
